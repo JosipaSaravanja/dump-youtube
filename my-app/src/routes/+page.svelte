4bccced5f6170e4bc760cb2a9c7c34adf5180705
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-09-13 03:46:21 +0200

    all languages

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'Homepage'
+</script>
+
+<div class="wrapper">
+  <h1>{title}</h1>
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+    font-weight: 800;
+    text-align: center;
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    align-self: center;
+    justify-content: center;
+  }
+</style>
