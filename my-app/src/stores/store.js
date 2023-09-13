deae3bc4ad1a965d2075e815918f41e611e19108
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-09-13 05:08:22 +0200

    localstorage

--- my-app/src/stores/store.js
+++ my-app/src/stores/store.js
@@ -1,5 +1,20 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 let data=['data1', 'data2']
 
 export const list = writable(data);
+
+if(browser && JSON.parse(window.localStorage.getItem('list'))){
+	data =JSON.parse(window.localStorage.getItem('list'))
+}else{
+	data =[]
+}
+
+export const todos = writable(data);
+
+todos.subscribe((value) => {
+	if (browser) {
+		localStorage.setItem('list', JSON.stringify(value));
+	}
+});
