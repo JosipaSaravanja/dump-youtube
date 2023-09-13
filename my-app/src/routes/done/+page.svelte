25ab757ca6812b162c7ee36bd99ec63010c85c64
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-09-13 20:42:18 +0200

    todo and done(withut css)

---
+++ my-app/src/routes/done/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {list} from "../../stores/store";
+    import Card from "../../components/Card.svelte";
+  </script>
+  
+  <div>
+    {#each $list.filter((todo) => todo.isFinished) as props}
+      <Card {props} />
+    {/each}
+  </div>
+  
